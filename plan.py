bbc72b1cdb303593ce3407fce4127f6f1e8762cd
Bacon <19910101bacon@gmail.com>
2019-11-16 20:36:40 +0800

    tem

--- plan.py
+++ plan.py
@@ -10,7 +10,7 @@ for i in range(5):
     print('-'*30 + command + '-'*30)
     print('\n')
     os.system(command)
-sys.exit()
+
 for i in range(500):
     command = 'python3.7 training_model.py {0} {1} {2}'.format(i, symbol, target_length)
     print('-'*80)
